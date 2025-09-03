28d3e3ee87b15d8f30277bcc894e3c0d66f21ba7
Alex Banica <ionut.alexandru.banica@gmail.com>
2025-09-03 10:29:26 +0200

    Initialize Maven-based Spring Boot project with initial setup

---
+++ src/main/java/com/alexlab/pi/deviceintegrationapi/DeviceIntegrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.alexlab.pi.deviceintegrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeviceIntegrationApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeviceIntegrationApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alexlab/pi/deviceintegrationapi/DeviceIntegrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexlab.pi.deviceintegrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeviceIntegrationApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
