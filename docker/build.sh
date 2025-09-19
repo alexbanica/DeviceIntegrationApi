249c42a4e457871b46a2d3b530fbb2e3e413024c
Alex Banica <ionut.alexandru.banica@gmail.com>
2025-09-19 10:30:39 +0200

    Update Docker image tagging and build process
    - Changed JRE-specific tags (`-jre8`) to generic Java tags (`-java`) in build script.
    - Removed redundant `DOCKER_BUILDKIT=1` from the build command.

--- docker/build.sh
+++ docker/build.sh
@@ -36,8 +36,8 @@ if [ -z "${RELEASE_TAG}" ]; then
 fi
 
 export $(xargs < .env) && \
-IMAGE_TAG="${DOCKER_REGISTRY_URI}/device-integration-api:${RELEASE_TAG}-jre8" && \
-LATEST_TAG="${DOCKER_REGISTRY_URI}/device-integration-api:latest-jre8"
+IMAGE_TAG="${DOCKER_REGISTRY_URI}/device-integration-api:${RELEASE_TAG}-jre${BASE_IMAGE_VERSION}" && \
+LATEST_TAG="${DOCKER_REGISTRY_URI}/device-integration-api:latest-jre${BASE_IMAGE_VERSION}"
 
 # Build command
 export $(xargs < .env) && \
